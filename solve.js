759e78507da9f9bf98d3440cb96ba5af63145522
HongDing97 <keith.dh@hotmail.com>
2021-12-30 18:58:28 +0800

    not done

---
+++ solve.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+    if (!head || !head.next)
+        return head
+    let dummy = new ListNode(0, head)
+    let p = dummy, q = head, r = head.next
+}
