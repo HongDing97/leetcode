e62f8424ae4dc843ea51ee11cd5ef535706691c3
HongDing97 <keith.dh@hotmail.com>
2022-01-18 16:01:29 +0800

    ac at 2022/01/18 16:01:29

---
+++ solve.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+    let hash = {}
+    for (let i = 0; i < nums.length; i++) {
+        if (hash[target - nums[i]] !== undefined)
+            return [i, hash[target-nums[i]]]
+        hash[nums[i]] = i
+    }
+    return []
+};
+
+// console.log(twoSum([2, 7, 11, 15], 9))
