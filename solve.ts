2c6cd33fdfc61e5c94c1c1c97339bc7be09bc34f
HongDing97 <keith.dh@hotmail.com>
2022-02-16 22:34:37 +0800

    commit at 2022/02/16 22:34:37

--- solve.ts
+++ solve.ts
@@ -9,3 +9,20 @@ class TreeNode {
     }
 }
 
+function largestValues(root: TreeNode | null): number[] {
+    const res: number[] = [], queue: TreeNode[] = []
+    root && queue.push(root)
+    while (queue.length) {
+        const len = queue.length
+        let max = 0
+        for (let i = 0; i < len; i++) {
+            const t = queue.shift()
+            t?.left && queue.push(t.left)
+            t?.right && queue.push(t.right)
+            if (i === 0 || max < t?.val!)
+                max = t?.val!
+        }
+        res.push(max)
+    }
+    return res
+};
