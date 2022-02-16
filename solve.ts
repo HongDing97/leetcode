372669584fc361a89b17e21eef5711312ac9be4b
HongDing97 <keith.dh@hotmail.com>
2022-02-16 21:38:09 +0800

    commit at 2022/02/16 21:38:09

---
+++ solve.ts
@@ -0,0 +1,27 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+function levelOrder(root: TreeNode | null): number[][] {
+    const res: number[][] = [], queue: TreeNode[] = []
+    root && queue.push(root)
+    while (queue.length) {
+        const len = queue.length
+        const r: number[] = []
+        for (let i = 0; i < len; i++) {
+            const t = queue.shift()
+            r.push(t?.val!)
+            t?.left && queue.push(t.left)
+            t?.right && queue.push(t.right)
+        }
+        res.push(r)
+    }
+    return res
+};
