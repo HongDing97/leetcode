a33e56f82f6c3d587a6b86c64e23d84f871790bb
HongDing97 <keith.dh@hotmail.com>
2022-02-18 22:24:36 +0800

    update

--- solve.ts
+++ solve.ts
@@ -9,20 +9,33 @@ class TreeNode {
     }
 }
 
+// 递归
 function minDepth(root: TreeNode | null): number {
-    let deepth = 0
-    const queue: TreeNode[] = []
-    root && queue.push(root)
-    while (queue.length) {
-        const len = queue.length
-        deepth++
-        for (let i = 0; i < len; i++) {
-            const t = queue.shift()
-            if (!t?.left && !t?.right)
-                return deepth
-            t.left && queue.push(t.left)
-            t.right && queue.push(t.right)
-        }
-    }
-    return deepth
+    if (!root)  
+        return 0
+    const l = minDepth(root.left), r = minDepth(root.right)
+    if (root.left && !root.right)
+        return l + 1
+    if (!root.left && root.right)
+        return r + 1
+    return Math.min(l, r) + 1
 };
+
+// 层序遍历
+// function minDepth(root: TreeNode | null): number {
+//     let deepth = 0
+//     const queue: TreeNode[] = []
+//     root && queue.push(root)
+//     while (queue.length) {
+//         const len = queue.length
+//         deepth++
+//         for (let i = 0; i < len; i++) {
+//             const t = queue.shift()
+//             if (!t?.left && !t?.right)
+//                 return deepth
+//             t.left && queue.push(t.left)
+//             t.right && queue.push(t.right)
+//         }
+//     }
+//     return deepth
+// };
