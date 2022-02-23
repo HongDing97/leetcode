41bcce6c0538d6b5ac0f307878961feaa4590721
HongDing97 <keith.dh@hotmail.com>
2022-02-23 15:56:01 +0800

    commit at 2022/02/23 15:56:01

--- solve.ts
+++ solve.ts
@@ -25,3 +25,10 @@ function createTree(nodes: (number | null)[]): TreeNode | null {
     }
     return root
 }
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+    if (!root)  return null
+    if (val === root.val) return root
+    else if (val < root.val)    return searchBST(root.left, val)
+    else return searchBST(root.right, val)
+};
