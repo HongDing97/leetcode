7f87311f89ce4040d9d2273536b72b26d32f9a7a
HongDing97 <keith.dh@hotmail.com>
2022-02-27 18:32:05 +0800

    commit at 2022/02/27 18:32:05

--- solve.ts
+++ solve.ts
@@ -25,3 +25,12 @@ function createTree(nodes: (number | null)[]): TreeNode | null {
     }
     return root
 }
+
+function trimBST(root: TreeNode | null, low: number, high: number): TreeNode | null {
+    if (!root) return null
+    if (root.val < low) return trimBST(root.right, low, high)
+    if (root.val > high) return trimBST(root.left, low, high)
+    root.left = trimBST(root.left, low, high)
+    root.right = trimBST(root.right, low, high)
+    return root
+};
