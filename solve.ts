1324bc600d87fa5215b71dc735988abc72f68ef6
HongDing97 <keith.dh@hotmail.com>
2022-03-12 23:30:02 +0800

    commit at 2022/03/12 23:30:02

---
+++ solve.ts
@@ -0,0 +1,14 @@
+function maxProfit(prices: number[]): number {
+    const dp = new Array(prices.length).fill(0).map(e => new Array(3).fill(0))
+    dp[0][0] = -prices[0]
+    dp[0][1] = 0
+    dp[0][2] = 0
+    for (let i = 1; i < prices.length; i++) {
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] - prices[i])
+        dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+        dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1])
+    }
+    return Math.max(dp[dp.length - 1][1], dp[dp.length - 1][2])
+};
+
+maxProfit([1])
