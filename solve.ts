fb1edcae5a014a49c3c0be4ab709a0e83cdebe9e
HongDing97 <keith.dh@hotmail.com>
2022-03-12 22:37:07 +0800

    commit at 2022/03/12 22:37:07

--- solve.ts
+++ solve.ts
@@ -1,10 +1,24 @@
+// 贪心
+// function maxProfit(prices: number[]): number {
+//     let profit = 0
+//     for(let i = 0; i < prices.length - 1; i++) {
+//         if (prices[i + 1] > prices[i])
+//             profit += prices[i + 1] - prices[i]
+//     }
+//     return profit
+// };
+
+// dp
 function maxProfit(prices: number[]): number {
-    let profit = 0
-    for(let i = 0; i < prices.length - 1; i++) {
-        if (prices[i + 1] > prices[i])
-            profit += prices[i + 1] - prices[i]
+    const dp = new Array(prices.length).fill(0).map(e => new Array(2).fill(0))
+    dp[0][0] = -prices[0]
+    dp[0][1] = 0
+    for (let i = 1; i < prices.length; i++) {
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i])
+        dp[i][1] = Math.max(dp[i - 1][1], prices[i] + dp[i - 1][0])
     }
-    return profit
+    return dp[prices.length - 1][1]
 };
 
-maxProfit([1, 2, 3, 4, 5])
+
+maxProfit([7, 1, 5, 3, 6, 4])
