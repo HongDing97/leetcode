56a0276e7aa08f4f1d8367251440d97c28c34d64
HongDing97 <keith.dh@hotmail.com>
2022-03-13 17:55:22 +0800

    commit at 2022/03/13 17:55:22

--- solve.ts
+++ solve.ts
@@ -1,12 +1,27 @@
+// 贪心
+// function maxSubArray(nums: number[]): number {
+//     let sum = 0, max = -Infinity
+//     for (let i = 0; i < nums.length; i++) {
+//         sum += nums[i]
+//         if (sum > max)
+//             max = sum
+//         if (sum < 0)
+//             sum = 0
+//     }
+//     return max
+// };
+
+// dp
 function maxSubArray(nums: number[]): number {
-    let sum = 0, max = -Infinity
-    for (let i = 0; i < nums.length; i++) {
-        sum += nums[i]
-        if (sum > max)
-            max = sum
-        if (sum < 0)
-            sum = 0
+    const dp = new Array(nums.length).fill(0)
+    let res = nums[0]
+    dp[0] = nums[0]
+    for (let i = 1; i < nums.length; i++) {
+        dp[i] = Math.max(nums[i] + dp[i - 1], nums[i])
+        if (dp[i] >res)
+            res = dp[i]
     }
-    return max
+    return res
 };
 
+maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
